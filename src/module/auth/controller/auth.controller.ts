a5d84ca012788e0808d1e66a788f2911988c865a
Begeeeg <v.yecyec7@gmail.com>
2026-02-27 13:39:52 +0800

    ♻ refactor: generateToken and sign up

--- src/module/auth/controller/auth.controller.ts
+++ src/module/auth/controller/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { signUpService } from "../service/auth.service";
+import { generateTokenandSetCookie } from "../../../common/utils/generateTokenandSetCookie";
 
 export const signIn = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -13,7 +14,16 @@ export const signUp = async (req: Request, res: Response): Promise<void> => {
     try {
         const user = await signUpService(req.body);
 
-        res.status(201).json({ message: "User registered successfully", user });
+        generateTokenandSetCookie(res, user._id.toString());
+
+        res.status(201).json({
+            message: "User registered successfully",
+            data: {
+                userId: user._id.toString(),
+                username: user.username,
+            },
+        });
+        console.log("User registered successfully:", user);
     } catch (error: any) {
         res.status(error.statusCode || 500).json({ error: error.message });
         console.log("Erro signUp controller:", error);
