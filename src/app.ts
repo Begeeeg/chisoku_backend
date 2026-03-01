e8f65bff2af5ca6ef51ff41431236a174064f455
Begeeeg <v.yecyec7@gmail.com>
2026-03-01 16:04:16 +0800

    ♻ refactor(cors): adding connection with frontend

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { authRoutes } from "./module/auth";
 import { expenseRoutes } from "./module/expense";
 
 const app = express();
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
